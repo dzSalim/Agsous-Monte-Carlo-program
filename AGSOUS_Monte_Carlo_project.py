a977792749f04942780dee0e465257b1336242d7
Salim <salim.agsous99@gmail.com>
2023-09-13 17:34:58 +0200

    Ajustement du code et ajout de méthodes Changement de la fonction minimize_energy qui va itérer par acide-aminé Ajout argparse, changement de fichier ajout de couleur par type d'acide aminé

--- AGSOUS_Monte_Carlo_project.py
+++ AGSOUS_Monte_Carlo_project.py
@@ -1,15 +1,26 @@
 import random
 import math
+import os
 import matplotlib.pyplot as plt
+import argparse
+
 hydrophobic_aa = "FAMILYVWC"
 
-class Amino_acid :
+parser = argparse.ArgumentParser(description="REMC Protein Folding")
 
-    def __init__(self,numero,type,x=0.0,y=0.0):
-        if (type in hydrophobic_aa):
-            self.type = "H"
-        else:
-            self.type = "P"
+# Argument obligatoire pour spécifier le fichier FASTA
+parser.add_argument("fasta_file", help="Le fichier FASTA contenant la séquence de la protéine")
+
+# Argument optionnel pour spécifier le nombre d'itérations (par défaut à 20000)
+parser.add_argument("-n", "--iterations", type=int, default=10000,
+                    help="Le nombre d'itérations pour la minimisation d'énergie")
+
+args = parser.parse_args()
+
+class Amino_acid:
+
+    def __init__(self, numero, type, x=0.0, y=0.0):
+        self.type = type  # Type tel qu'il est lu à partir du fichier FASTA
         self.numero = numero
         self.x = x
         self.y = y
@@ -17,31 +28,28 @@ class Amino_acid :
     def __str__(self):
         return f"Il s'agit de l'acide aminé numéro {self.numero} de type {self.type} et de coordonnées [{self.x},{self.y}]"
 
-    def get_type (self):
+    def get_type(self):
         return self.type
 
-    def get_numero (self):
+    def get_numero(self):
         return self.numero
 
-    def get_coord (self):
+    def get_coord(self):
         return self.x, self.y
 
-    def set_coord (self,new_x,new_y):
+    def set_coord(self, new_x, new_y):
         self.x = new_x
         self.y = new_y
 
-
-
-class Protein :
-    def __init__(self,filename):
-       self.sequence_aa = self.lecture_seq(filename)
-       self.filename = filename
-       self.current_energy = 0
-       self.initial_structure()
+class Protein:
+    def __init__(self, filename):
+        self.sequence_aa = self.lecture_seq(filename)
+        self.filename = filename
+        self.current_energy = 0
+        self.initial_structure()
 
     def initial_structure(self):
         """ Initialise la structure initiale de la protéine en respectant les mouvements implémentés."""
-
         l = len(self.sequence_aa)
 
         # Place le premier acide aminé (n°1) à la position (0, 0)
@@ -51,40 +59,27 @@ class Protein :
         for i in range(1, l):
             prev_x, prev_y = self.sequence_aa[i - 1].get_coord()
             if i % 2 == 1:
-                # Pour les acides aminés impairs, ils seront placé a coté du précédent
+                # Pour les acides aminés impairs, ils seront placés à côté du précédent
                 self.sequence_aa[i].set_coord(prev_x + 1, prev_y)
             else:
-                # Pour les acides aminés pairs, ils sont placés au dessus du précédent
+                # Pour les acides aminés pairs, ils sont placés au-dessus ou en dessous du précédent
                 if i % 4 == 0:
                     self.sequence_aa[i].set_coord(prev_x, prev_y + 1)
                 else:
                     self.sequence_aa[i].set_coord(prev_x, prev_y - 1)
 
-    def lecture_seq(self,filename):
-        """Fonction qui permet de lire un fichier fasta et de traduire les acides aminées
-        selon le modèle HP, cette fonction crée également un nouveau fichier au format fasta
-        avec les acides aminées modifiées.
-
-        args :
-        - filename : nom du fichier fasta """
-
-        prot_formatee = ""
+    def lecture_seq(self, filename):
+        """Fonction qui permet de lire un fichier FASTA et de créer la séquence d'acides aminés sans modification de type."""
         sequence = []
         with open(filename, "r") as fasta:
-            with open(filename[:-6] + '_HP' + filename[-6:], "w") as out:
-                for line in fasta:
-                    if not line.startswith(">"):
-                        for aa in line.strip():
-                            if aa in hydrophobic_aa:
-                                aa = "H"
-                            else:
-                                aa = "P"
-                            out.write(f'{aa}')
-                            prot_formatee = prot_formatee + aa
-
-        for indice, aa in enumerate(prot_formatee):
-            sequence.append(Amino_acid(indice + 1, aa, x=indice, y=0))
-
+            for line in fasta:
+                if not line.startswith(">"):
+                    for aa in line.strip():
+                        if aa in hydrophobic_aa:
+                            aa_type = 'H'
+                        else:
+                            aa_type = 'P'
+                        sequence.append(Amino_acid(len(sequence) + 1, aa_type))
         return sequence
 
 
@@ -163,7 +158,12 @@ class Protein :
         #Calcul de l'energie post-movement
         self.current_energy = self.energetic_measure()
 
+        if self.sequence_aa[i].get_type() == 'H':
+            self.sequence_aa[i].type = 'H'
+
         return "Mouvement possible"
+
+
 
 
     def end_move(self):
@@ -210,7 +210,13 @@ class Protein :
         #Calcul de l'energie post-movement
         self.current_energy = self.energetic_measure()
 
+        if end_to_move == 1 and self.sequence_aa[0].get_type() == 'H':
+            self.sequence_aa[0].type = 'H'
+        elif end_to_move == n and self.sequence_aa[-1].get_type() == 'H':
+            self.sequence_aa[-1].type = 'H'
+
         return "Mouvement possible"
+
 
 
     def crankshaft_move(self):
@@ -244,9 +250,14 @@ class Protein :
         # Calcul de l'energie post-movement
         self.current_energy = self.energetic_measure()
 
+        if self.sequence_aa[i].get_type() == 'H':
+            self.sequence_aa[i].type = 'H'
+        if self.sequence_aa[i + 1].get_type() == 'H':
+            self.sequence_aa[i + 1].type = 'H'
+
         return "Mouvement possible"
 
-    def minimize_energy(self, max_iterations=100):
+    def minimize_energy(self, max_iterations=args.iterations):
         """Minimise l'énergie globale de la protéine en effectuant des mouvements aléatoires par acide aminé."""
         best_energy = self.energetic_measure()  # Énergie initiale
         best_sequence = list(self.sequence_aa)  # Configuration initiale
@@ -282,13 +293,16 @@ class Protein :
         self.sequence_aa = list(best_sequence)
         self.current_energy = best_energy
 
-    def visualize_sequence_2d(self):
+
+    def visualize_sequence_2d(self, save_plot=False):
+        """Cette fonction permet de visualiser la séquence 2D de la conformation de la protéine """
         plt.figure()
         x = [aa.get_coord()[0] for aa in self.sequence_aa]
         y = [aa.get_coord()[1] for aa in self.sequence_aa]
 
-
-        plt.plot(x, y, 'o', markersize=10, markerfacecolor='lightgray', markeredgecolor='black')
+        for aa in self.sequence_aa:
+            color = 'red' if aa.get_type() == 'H' else 'blue'
+            plt.plot(aa.x, aa.y, 'o', markersize=10, markerfacecolor=color, markeredgecolor='black')
 
         # Ajout de lignes entre les acides aminés
         for i in range(len(self.sequence_aa) - 1):
@@ -301,18 +315,30 @@ class Protein :
         plt.ylabel('Position Y')
         plt.grid(True)
 
-        # Affichage de la grille
+        if save_plot:
+            if not os.path.exists("result"):
+                os.makedirs("result")
+
+            # Sauvegardez le plot au format PNG dans le dossier "result"
+            plt.savefig(os.path.join("result", "sequence_2d.png"))
+
+        # Affichage du plot
         plt.show()
 
+
+
 if __name__ == "__main__":
-    protein = Protein("P02776.fasta")
+    args = parser.parse_args()
+    protein = Protein(args.fasta_file)
+    print(f"Utilisation du fichier FASTA : {args.fasta_file}")
+    print(f"Nombre d'itérations : {args.iterations}")
+
     print(protein.full_seq())
     print(protein.energetic_measure())
     protein.initial_structure()
+    protein.minimize_energy(max_iterations=args.iterations)
     protein.visualize_sequence_2d()
-    protein.minimize_energy()
-    protein.visualize_sequence_2d()
-    print(f" l'energie minimale est {protein.current_energy}")
+    print(f"L'énergie minimale est {protein.current_energy}")
 
 
 
